227fd46c80df4b58cd7395ee49632e8bfbee2da3
MatteoCalonaci <matteo.calonaci.1994@gmail.com>
2024-06-13 16:47:58 +0200

    first commit

---
+++ api-client/main.js
@@ -0,0 +1,21 @@
+// VUE JS SCRIPT
+// --------------------------------
+const { createApp } = Vue
+createApp({
+    data() {
+        return {
+        }
+    },
+
+    methods: {
+
+
+    },
+
+
+    //MOMENTI DI BASE
+    mounted() {
+        console.log("app montata");
+
+    }
+}).mount("#app")
